bc52acffc9143626bd2767540375fc7bde391fcd
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-20 18:53:32 -0600

    write test 3; test not passing
    Co-authored-by: DeAunte Hall <godsofolympus88@gmail.com>
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>

--- README.md
+++ README.md
@@ -18,12 +18,15 @@ _{This is a detailed description of your application. Give as much detail as nee
 ## Specifications
 
 Describe: `pigLatin()`
-Test: "It will add 'way' to the end of words that begin with a vowel"
+Test-1: "It will add 'way' to the end of words that begin with a vowel"
 Expect(pigLatin("a")).toEqual("away");
 
-Test: "It will check if first character of input string is a vowel, before adding 'way'"
+Test-2: "It will check if first character of input string is a vowel, before adding 'way'"
 Expect(pigLatin("broken")).toEqual("broken");
 Expect(pigLatin("abracadabra")).toEqual("abracadabraway");
+
+Test-3: "If first character is consonant, it will return first and/or subsequent consonants until a vowel reached"
+Expect:(pigLatin("grab")).toEqual("gr");
 
 
 ## Setup/Installation Requirements
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 // Describe: `pigLatin()`
-// Test: "It will add 'way' to the end of words that begin with a vowel"
+// Test-1: "It will add 'way' to the end of words that begin with a vowel"
 // Expect(pigLatin("a")).toEqual("away");
 
-// Test: "It will check if first character of input string is a vowel, before adding 'way'"
+// Test-2: "It will check if first character of input string is a vowel, before adding 'way'"
 // Expect(pigLatin("broken")).toEqual("broken");
 // Expect(pigLatin("abracadabra")).toEqual("abracadabraway");
+
+//Test-3: "If first character is consonant, it will return first and/or subsequent consonants until a vowel reached"
+//Expect:(pigLatin("grab")).toEqual("gr");
 
 function pigLatin(words) {
   let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
@@ -12,7 +15,11 @@ function pigLatin(words) {
   for (let i = 0; i < vowels.length; i++) {
     if (words[0] === vowels[i]) {
       pigLatinPhrase += "way";
-    }
+      return pigLatinPhrase;
+    } else if (words[1] === vowels[i]) {
+      const consonantCluster = words.slice(0,i);
+      return consonantCluster;
+    }    
   }
   return pigLatinPhrase;
 }
